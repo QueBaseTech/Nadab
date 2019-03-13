cb7dc641bb2b8d645d44703ed66b2031462a7a60
nyugoh <nyugoh@gmail.com>
2019-03-13 16:01:03 +0300

    🐛 Editing product item

--- app/src/main/java/com/example/karokojnr/nadab_hotels/model/Product.java
+++ app/src/main/java/com/example/karokojnr/nadab_hotels/model/Product.java
@@ -3,24 +3,23 @@ package com.example.karokojnr.nadab_hotels.model;
 import com.google.gson.annotations.SerializedName;
 
 public class Product {
-
-//    @SerializedName("data")
-//    private List<Product> products;
+    @SerializedName("_id")
+    private String id;
 
     @SerializedName("name")
     private String name;
+
     @SerializedName("unitMeasure")
     private String unitMeasure;
+
     @SerializedName("price")
     private String price;
+
     @SerializedName("hotel")
     private String hotel;
+
     @SerializedName("image")
     private String image;
-//    @SerializedName("sellingStatus")
-//    private String sellingStatus;
-//    @SerializedName("servedWith")
-//    private String servedWith;
 
     public Product(String name, String unitMeasure, String price) {
         this.name = name;
@@ -28,12 +27,11 @@ public class Product {
         this.price = price;
         this.hotel = hotel;
         this.image = image;
-//        this.sellingStatus = sellingStatus;
-        //this.servedWith =  servedWith;
     }
-//    @SerializedName("genre")
-//    private List<String> genre;
 
+    public String getId() {
+        return id;
+    }
 
     public String getName() {
         return name;
